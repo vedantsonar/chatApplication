2d4ad8c15f9dc75521c626b65cc508e040e5eacc
vedantsonar <sonarvedant23@gmail.com>
2024-08-27 16:19:12 +0530

    Files Updated

---
+++ Server/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// routes import
+
+
+// route declaration
+
+
+
+export { app }
---
+++ Server/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import { app } from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err)
+})
