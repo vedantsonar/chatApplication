11524ff07f3508d62a93547bb05c3af3a7d30872
vedantsonar <sonarvedant23@gmail.com>
2024-08-29 13:52:55 +0530

    Files Updates

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+     <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [
+  daisyui,
+];
